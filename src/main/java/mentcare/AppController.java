3cb6799806fcd93e518673d06af49ecad9fc7dbe
Mathias Dalla Palma <mathiasdallapalma@gmail.com>
2024-01-16 14:42:53 +0100

    Initialize project

---
+++ src/main/java/mentcare/AppController.java
@@ -0,0 +1,10 @@
+package mentcare;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class AppController {
+
+
+
+}
---
+++ src/test/java/mentcare/SystemTest.java
@@ -0,0 +1,49 @@
+package mentcare;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.apache.commons.lang3.SystemUtils;
+
+import org.openqa.selenium.WebDriver;
+
+import java.nio.file.Paths;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+public class SystemTest {
+
+    private WebDriver driver;
+    @Before
+    public void setUp() {
+        org.openqa.selenium.chrome.ChromeOptions chrome_options = new ChromeOptions();
+        chrome_options.addArguments("--headless");
+        if(SystemUtils.IS_OS_WINDOWS){
+            System.setProperty("webdriver.chrome.driver",
+                    Paths.get("src/test/resources/chromedriver_win32_96/chromedriver.exe").toString());
+        }
+        else if (SystemUtils.IS_OS_MAC){
+            System.setProperty("webdriver.chrome.driver",
+                    Paths.get("src/test/resources/chromedriver_mac64_96/chromedriver").toString());
+        }
+        else if (SystemUtils.IS_OS_LINUX){
+            System.setProperty("webdriver.chrome.driver",
+                    Paths.get("src/test/resources/chromedriver_linux64_96/chromedriver").toString());
+        }
+        if (driver == null)
+            driver = new ChromeDriver(chrome_options);
+    }
+
+    @After
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+
+}
