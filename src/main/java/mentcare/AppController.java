1c82e5b02c3cf4b9f2cdeea15785ea95d8b4dd14
Alessandro Pallotta <flowerbot00@protonmail.com>
2024-01-20 20:29:37 +0100

    page for add Prescription

--- src/main/java/mentcare/AppController.java
+++ src/main/java/mentcare/AppController.java
@@ -1,17 +1,27 @@
 package mentcare;
 
 import mentcare.models.Patient;
+import mentcare.models.Prescription;
 import mentcare.repositories.PatientRepository;
+import mentcare.repositories.PrescriptionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Optional;
 
 @Controller
 public class AppController {
 
     @Autowired
-    private PatientRepository repository;
+    private PatientRepository patientRepository;
+
+    @Autowired
+    private PrescriptionRepository prescriptionRepo;
 
     @RequestMapping("/")
     public String index(){
@@ -32,6 +42,36 @@ public class AppController {
         return "redirect:/list";
     }
     */
+
+    @RequestMapping("/addPrescription/{idPatient}")
+    public String addPrescription(Model model, @PathVariable(name="idPatient")Long idPatient){
+        Optional<Patient> opt = patientRepository.findById(idPatient);
+        if(opt.isPresent()){
+            model.addAttribute("idPatient",idPatient);
+            model.addAttribute("name", opt.get().getFirstname() + " " + opt.get().getLastname());
+            model.addAttribute("prescriptions", prescriptionRepo.findByPatientID(idPatient));
+            return "addPatient";
+        }else {
+            model.addAttribute("error", "Il paziente con id ["+idPatient.toString()+"] Non è stato trovato :(\n");
+            return "error";
+        }
+    }
+
+    @PostMapping("/addPrescription/{idPatient}")
+    public String addPrescription(Model model, @PathVariable(name="idPatient")Long idPatient,
+                                  @RequestParam(name="drug")String drug,
+                                  @RequestParam(name="quantity")String quantity,
+                                  @RequestParam(name="note")String note){
+        try{
+            Integer quant = Integer.getInteger(quantity);
+            Prescription prescr = new Prescription(drug, quant, note, idPatient);
+            prescriptionRepo.save(prescr);
+            return "patientView/"+idPatient.toString();
+        }catch (NumberFormatException e){
+            model.addAttribute("error","e");
+            return "error";
+        }
+    }
 
 
 }
